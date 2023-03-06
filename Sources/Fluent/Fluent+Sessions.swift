99be8c51840bf77c2171b3309374bc52722d447b
Zanoroy <zanoroy@gmail.com>
2023-03-06 13:21:10 +1030

    Changed from _fluent_session to websession Added clientIP to session table

--- Sources/Fluent/Fluent+Sessions.swift
+++ Sources/Fluent/Fluent+Sessions.swift
@@ -1,4 +1,5 @@
 import Vapor
+import FluentPostgresDriver
 
 extension Application.Fluent {
     public var sessions: Sessions {
@@ -51,25 +52,25 @@ extension Application.Sessions.Provider {
 
 private struct DatabaseSessions: SessionDriver {
     let databaseID: DatabaseID?
-    
+
     init(databaseID: DatabaseID? = nil) {
         self.databaseID = databaseID
     }
-    
+
     func createSession(_ data: SessionData, for request: Request) -> EventLoopFuture<SessionID> {
         let id = self.generateID()
         return SessionRecord(key: id, data: data)
             .create(on: request.db(self.databaseID))
             .map { id }
     }
-    
+
     func readSession(_ sessionID: SessionID, for request: Request) -> EventLoopFuture<SessionData?> {
         SessionRecord.query(on: request.db(self.databaseID))
             .filter(\.$key == sessionID)
             .first()
             .map { $0?.data }
     }
-    
+
     func updateSession(_ sessionID: SessionID, to data: SessionData, for request: Request) -> EventLoopFuture<SessionID> {
         SessionRecord.query(on: request.db(self.databaseID))
             .filter(\.$key == sessionID)
@@ -77,13 +78,13 @@ private struct DatabaseSessions: SessionDriver {
             .update()
             .map { sessionID }
     }
-    
+
     func deleteSession(_ sessionID: SessionID, for request: Request) -> EventLoopFuture<Void> {
         SessionRecord.query(on: request.db(self.databaseID))
             .filter(\.$key == sessionID)
             .delete()
     }
-    
+
     private func generateID() -> SessionID {
         var bytes = Data()
         for _ in 0..<32 {
@@ -108,38 +109,82 @@ private struct DatabaseSessionAuthenticator<User>: SessionAuthenticator
 }
 
 public final class SessionRecord: Model {
-    public static let schema = "_fluent_sessions"
+    public static let schema = "websession"
 
     struct Create: Migration {
-        func prepare(on database: Database) -> EventLoopFuture<Void> {
-            database.schema("_fluent_sessions")
+      func prepare(on database: Database) -> EventLoopFuture<Void> {
+        return
+          database.schema("websession")
                 .id()
                 .field("key", .string, .required)
                 .field("data", .json, .required)
+                .field("clientip", .string)
+                .field("created", .datetime, .required, .custom("DEFAULT now()"))
+                .field("modified", .datetime, .required, .custom("DEFAULT now()"))
                 .unique(on: "key")
-                .create()
+                .create().flatMap {
+              let sql = database as! SQLDatabase
+              // Create the modified update function
+              _ = sql.raw(SQLQueryString("CREATE OR REPLACE FUNCTION update_modified_column() " +
+                                        "RETURNS TRIGGER AS $$ " +
+                                        "BEGIN " +
+                                        "   IF row(NEW.*) IS DISTINCT FROM row(OLD.*) THEN " +
+                                        "      NEW.modified = now(); " +
+                                        "      RETURN NEW; " +
+                                        "   ELSE " +
+                                        "      RETURN OLD; " +
+                                        "   END IF; " +
+                                        "END; " +
+                                        "$$ language 'plpgsql';")).run()
+
+              // Create the modified update trigger
+              _ = sql.raw(SQLQueryString("CREATE TRIGGER session_modified_timestamp_update " +
+                                        "BEFORE UPDATE " +
+                                        "ON websession " +
+                                        "FOR EACH ROW " +
+                                        "EXECUTE PROCEDURE update_modified_column();")).run()
+
+              _ = sql.raw(SQLQueryString("COMMENT ON TABLE public.\"websession\" IS 'Active Web Session'.;")).run()
+              _ = sql.raw(SQLQueryString("COMMENT ON COLUMN public.\"websession\".id IS 'Unique key for the record.';")).run()
+              _ = sql.raw(SQLQueryString("COMMENT ON COLUMN public.\"websession\".key IS 'Unique identifier to the users session.';")).run()
+              _ = sql.raw(SQLQueryString("COMMENT ON COLUMN public.\"websession\".data IS 'Session specific data';")).run()
+              _ = sql.raw(SQLQueryString("COMMENT ON COLUMN public.\"websession\".clientip IS 'Client''s IP address';")).run()
+              _ = sql.raw(SQLQueryString("COMMENT ON COLUMN public.\"websession\".created IS 'When the session row was created';")).run()
+              return sql.raw(SQLQueryString("COMMENT ON COLUMN public.\"websession\".modified IS 'The last time the session row was updated';")).run()
+            }
         }
 
-        func revert(on database: Database) -> EventLoopFuture<Void> {
-            database.schema("_fluent_sessions").delete()
-        }
+    // Undo the change made in `prepare`, if possible.
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+      return
+        database.schema(SessionRecord.schema).delete()
     }
+   }
 
     public static var migration: Migration {
         Create()
     }
-    
+
     @ID(key: .id)
     public var id: UUID?
-    
+
     @Field(key: "key")
     public var key: SessionID
-    
+
     @Field(key: "data")
     public var data: SessionData
-    
+
+    @OptionalField(key: "clientip")
+    var clientip: String?
+
+    @OptionalField(key: "created")
+    var created: Date?
+
+    @OptionalField(key: "modified")
+    var modified: Date?
+
     public init() { }
-    
+
     public init(id: UUID? = nil, key: SessionID, data: SessionData) {
         self.id = id
         self.key = key
